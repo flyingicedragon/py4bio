0950a659a1d2913e087900a72d93102cdbc69baa
icedragon <flyingicedragon@qq.com>
2022-07-11 23:14:08 +0800

    change docstring format
    Signed-off-by: icedragon <flyingicedragon@qq.com>

--- src/py4bio/processor/bedtools.py
+++ src/py4bio/processor/bedtools.py
@@ -2,15 +2,17 @@ import subprocess
 
 
 def bedtools(cmd: list) -> str:
-    '''Spawn new process to use bedtools. It is a universal Popen maker but is
+    """Spawn new process to use bedtools. It is a universal Popen maker but is
     adapted to bedtools.
-    cmd: command args list containing 'bedtools', eg,
-
+    :param cmd: command args list containing 'bedtools', eg,
     ['bedtools', 'window',
     '-a', bed_path: str,
     '-b', gff_path: str,
     '-w', str(flank_size: int)]
-    '''
+    :type cmd: list
+    :return: bedtools output
+    :rtype: str
+    """
     sp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     outs, errs = sp.communicate()
     sp.wait()
